5e02874c893f6ca4a4800a09d1038888b4fc016b
almogg <almog.geva@audiocodes.com>
2022-08-27 17:11:45 +0300

    added app.fxml that includes headerComponent.fxml component
    + code cleanup

--- user-interface/src/main/java/handler/PropertiesService.java
+++ user-interface/src/main/java/handler/PropertiesService.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @EqualsAndHashCode
 public class PropertiesService {
     @Getter private static final String log4jPropertiesResourcePath = "/main/resources/log4j.properties";
-    @Getter private static final String headerFxmlPath = "/src/main/resources/subcomponents/headerGrid.fxml";
+    @Getter private static final String headerFxmlPath = "/src/main/resources/subcomponents/headerComponent.fxml";
     @Getter private static final String appFxmlPath = "/src/main/resources/app.fxml";
     @Getter private static final String primarySceneFxmlPath = headerFxmlPath;
 }
